7e84bb6e9506462a167cb72433196de828183dd6
Miruna Popescu <miru.popescu@yahoo.fr>
2021-09-27 15:40:36 +0200

    creation projet Spring Boot

---
+++ src/main/java/co/simplon/GymnaseApplication.java
@@ -0,0 +1,13 @@
+package co.simplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymnaseApplication.class, args);
+	}
+
+}
